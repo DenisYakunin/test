efe988373fae050ca0d0ff24e3eef568f33eedd4
Denis Yakunin <denis.yakunin9856@gmail.com>
2023-10-27 12:44:29 +0300

    add db

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -1,28 +1,29 @@
 <?php
 
-use App\Core\Controller;
-use App\Lib\AppException;
-use App\Lib\Validators\ProductSchemeValidator;
-use App\Lib\Validators\ProductValidator;
-use App\Lib\RequestDataExtractor;
+use src\Core\Controller;
+use src\Lib\AppException;
+use src\Lib\ProductMapper;
+use src\Lib\Validators\ProductSchemeValidator;
+use src\Lib\Validators\ProductValidator;
+use src\Lib\RequestDataExtractor;
 
 class ProductController extends Controller
 {
-    private Storage $storage;
+    private ProductMapper $db;
     private Product $product;
     private ProductValidator $productValidator;
     private RequestDataExtractor $requestDataExtractor;
     private ProductSchemeValidator $productSchemeValidator;
 
     public function __construct(
-        Storage $storageModel,
+        ProductMapper $db,
         Product $productModel,
         ProductValidator $productValidator,
         ProductSchemeValidator $productSchemeValidator,
         RequestDataExtractor $requestDataExtractor
     ) {
         parent::__construct();
-        $this->storage = $storageModel;
+        $this->db = $db;
         $this->product = $productModel;
         $this->productValidator = $productValidator;
         $this->requestDataExtractor = $requestDataExtractor;
@@ -33,7 +34,7 @@ class ProductController extends Controller
      * @throws AppException
      */
     public function getProducts(): void {
-        $products = $this->storage->getProducts();
+        $products = $this->db->selectAllProducts();
         $this->view->render('Products/default', $products);
     }
 
@@ -48,43 +49,42 @@ class ProductController extends Controller
         $this->product->createProduct($extractedParams);
         $this->productValidator->isValidProduct($this->product);
 
-        $products = $this->storage->getProducts();
-        $products[] = $this->product;
-        $this->storage->saveProducts($products);
+        $this->db->insertProduct($this->product);
 
-        $this->view->render('Products/default', $products);
+        $this->getProducts();
     }
 
     /**
      * @throws AppException
      */
     public function getProduct(int $id): void {
-        $products = $this->storage->getProducts();
-        if (!isset($products[$id])) {
+        $products = $this->db->selectProductById($id);
+        if (empty($products)) {
             throw new AppException('Некоректный id', 400);
         }
-        $this->view->render('Products/default', $products[$id]);
-    }
-
-    /**
-     * @throws AppException
-     */
-    public function deleteProduct(int $id): void {
-        $products = $this->storage->getProducts();
-        if (!isset($products[$id])) {
-            throw new AppException('Некоректный id', 400);
-        }
-        unset($products[$id]);
-        $this->storage->saveProducts($products);
         $this->view->render('Products/default', $products);
     }
 
     /**
      * @throws AppException
      */
+    public function deleteProduct(int $id): void {
+        $products = $this->db->selectProductById($id);
+        if (empty($products)) {
+            throw new AppException('Некоректный id', 400);
+        }
+
+        $this->db->deleteProduct($id);
+
+        $this->getProducts();
+    }
+
+    /**
+     * @throws AppException
+     */
     public function updateProduct(int $id): void {
-        $products = $this->storage->getProducts();
-        if (!isset($products[$id])) {
+        $products = $this->db->selectProductById($id);
+        if (empty($products)) {
             throw new AppException('Некоректный id', 400);
         }
 
@@ -94,18 +94,17 @@ class ProductController extends Controller
         $this->product->createProduct($extractedParams);
         $this->productValidator->isValidProduct($this->product);
 
-        $products[$id] = $this->product;
-        $this->storage->saveProducts($products);
+        $this->db->updateProduct($id, $this->product);
 
-        $this->view->render('Products/default', $products);
+        $this->getProducts();
     }
 
     /**
      * @throws AppException
      */
     public function updateProductParam(int $id): void {
-        $products = $this->storage->getProducts();
-        if (!isset($products[$id])) {
+        $products = $this->db->selectProductById($id);
+        if (empty($products)) {
             throw new AppException('Некоректный id', 400);
         }
 
@@ -113,20 +112,19 @@ class ProductController extends Controller
 
         if (isset($extractedParams['price'])) {
             $this->productValidator->isValidPrice($extractedParams['price']);
-            $products[$id]['price'] = $extractedParams['price'];
+            $this->db->updateProductPrice($id, $extractedParams['price']);
         }
 
-        if (isset($extractedParams['amount_available'])) {
-            $this->productValidator->isValidAmount($extractedParams['amount_available']);
-            $products[$id]['amount_available'] = $extractedParams['amount_available'];
-        }
-
-        if (isset($extractedParams['status'])) {
-            $this->productValidator->isValidStatus($extractedParams['status']);
-            $products[$id]['status'] = $extractedParams['status'];
-        }
-        $this->storage->saveProducts($products);
-        $this->view->render('Products/default', $products);
+//        if (isset($extractedParams['amount_available'])) {
+//            $this->productValidator->isValidAmount($extractedParams['amount_available']);
+//            $products[$id]['amount_available'] = $extractedParams['amount_available'];
+//        }
+//
+//        if (isset($extractedParams['status'])) {
+//            $this->productValidator->isValidStatus($extractedParams['status']);
+//            $products[$id]['status'] = $extractedParams['status'];
+//        }
+        $this->getProducts();
     }
 
     /**
@@ -136,8 +134,7 @@ class ProductController extends Controller
         if (!$this->hasAccess(false)) {
             throw new AppException('Требуется авторизация', 401);
         }
-        $products = $this->storage->getProducts();
-        $this->view->render('Products/default', $products);
+        $this->getProducts();
     }
 
     private function hasAccess(bool $access): bool {
---
+++ src/Lib/ProductMapper.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace src\Lib;
+use PDO;
+use Product;
+
+class ProductMapper
+{
+    private PDO $connection;
+    public function __construct($host, $user, $password, $database) {
+        $this->connection = new PDO("pgsql:host=$host;dbname=$database;port=5432;", $user, $password);
+    }
+
+    public function selectAllProducts(): array
+    {
+        $statement = $this->connection->prepare('SELECT * FROM products');
+        $statement->execute();
+        return $statement->fetchAll();
+    }
+
+    public function selectProductById(int $id): array
+    {
+        $statement = $this->connection->prepare('SELECT * FROM products WHERE id = :id');
+        $statement->execute(['id' => $id]);
+        return $statement->fetchAll();
+    }
+
+    public function insertProduct(Product $product): void
+    {
+        $statement = $this->connection->prepare('INSERT INTO products (code, price, name, description) VALUES (:code, :price, :name, :description)');
+        $statement->execute(['code' => $product->code, 'price' => $product->price, 'name' => $product->name, 'description' => $product->description]);
+    }
+
+    public function deleteProduct(int $id): void
+    {
+        $statement = $this->connection->prepare('DELETE FROM products WHERE id = :id');
+        $statement->execute(['id' => $id]);
+    }
+
+    public function updateProduct(int $id, Product $product): void
+    {
+        $statement = $this->connection->prepare('UPDATE products SET code = :code, price = :price, name = :name, description = :description WHERE id = :id');
+        $statement->execute(['id' => $id, 'code' => $product->code, 'price' => $product->price, 'name' => $product->name, 'description' => $product->description]);
+    }
+
+    public function updateProductPrice(int $id, string $price): void
+    {
+        $statement = $this->connection->prepare('UPDATE products SET price = :price WHERE id = :id');
+        $statement->execute(['id' => $id,'price' => $price]);
+    }
+}
